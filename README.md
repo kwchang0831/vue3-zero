af2a79f817de16d1cb18f882272e358c403e0c60
kwchang0831 <88667293+kwchang0831@users.noreply.github.com>
2023-11-26 20:01:47 +0800

    :rocket: Init

---
+++ README.md
@@ -0,0 +1,36 @@
+# vue3-zero
+
+with Pnpm + UnoCSS + ElementPlus
+
+## 使用
+
+```sh
+npx degit kwchang0831/vue3-zero#main my-app
+```
+
+## 環境
+
+- node: 21.2.0
+- npm: 10.2.4
+- pnpm: 8.11.0
+- element-plus: 2.4.2
+
+## 執行
+
+安裝 pnpm
+
+```sh
+npm i -g pnpm
+```
+
+安裝
+
+```sh
+pnpm i
+```
+
+本地啟動
+
+```sh
+pnpm dev
+```
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ElMessage } from 'element-plus'
+
+const mOpen = () => {
+  ElMessage('this is a message.')
+}
+</script>
+
+<template>
+  <div class="flex flex-col items-center gap4">
+    <h1 class="text-3xl font900">Hello World</h1>
+    <div class="font700 text-red">Sample Text</div>
+    <el-button type="primary" class="px4 py2"> ElementUI Button </el-button>
+
+    <el-button :plain="true" @click="mOpen">訊息提示</el-button>
+  </div>
+</template>
