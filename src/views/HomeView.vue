f8cebd254bb0d35317ee25183ec61670002d5e63
kwchang0831 <88667293+kwchang0831@users.noreply.github.com>
2023-11-26 20:43:28 +0800

    :wrench: Fixed: setup husky & gitmoji

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,8 +10,6 @@ const mOpen = () => {
   <div class="flex flex-col items-center gap4">
     <h1 class="text-3xl font900">Hello World</h1>
     <div class="font700 text-red">Sample Text</div>
-    <el-button type="primary" class="px4 py2"> ElementUI Button </el-button>
-
-    <el-button :plain="true" @click="mOpen">訊息提示</el-button>
+    <el-button type="primary" class="px4 py2" @click="mOpen">訊息提示</el-button>
   </div>
 </template>
